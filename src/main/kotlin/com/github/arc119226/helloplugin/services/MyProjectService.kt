53b0a32ff0984f717fb6a300ea974c889a28067c
GitHub Action <action@github.com>
2023-05-20 00:53:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/arc119226/helloplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arc119226/helloplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.arc119226.helloplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arc119226.helloplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
